ab5c877e554c00b74fa8a51ad32206f0e4ab1b57
Daniel Samuel Etukudo <danielsamueletukudo@Daniels-MacBook-Pro.local>
2023-11-03 00:48:52 +0100

    lol

--- save_encoding.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import face_recognition
-
-def save_encoding(path,image_name):
-            face_encoding= face_recognition.face_encodings(path,None,100,"large")[0]
-            with open(f"face_embaddings/{image_name}.dat", "wb") as data:
-               pickle.dump(face_encoding,data)
-
